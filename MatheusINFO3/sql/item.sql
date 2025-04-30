279598f49cf3fd8524a59f0f6d28d57d781e79da
Matheus Gonçalves <136489168+Theus1234567@users.noreply.github.com>
2025-04-30 11:11:10 -0300

    Add files via upload

--- MatheusINFO3/sql/item.sql
+++ MatheusINFO3/sql/item.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 29/04/2025 às 04:21
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Tempo de geração: 30-Abr-2025 às 07:32
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `item`
+-- Estrutura da tabela `item`
 --
 
 CREATE TABLE `item` (
@@ -35,33 +35,35 @@ CREATE TABLE `item` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `item`
+-- Extraindo dados da tabela `item`
 --
 
 INSERT INTO `item` (`id`, `nome`, `qtd`, `img`) VALUES
 (8, '', 0, ''),
 (11, 'Voltfriut', 2, 'https://img.game8.co/3685886/931ababd0b7b51191478d43c13fe0b22.png/show'),
-(12, 'Bomb Flower', 3, 'https://img.game8.co/3685882/df1ef2c4de6ea71f44ae77b01bf65465.png/show');
+(12, 'Bomb Flower', 3, 'https://img.game8.co/3685882/df1ef2c4de6ea71f44ae77b01bf65465.png/show'),
+(13, 'Apple', 2, 'https://img.game8.co/3685719/c67a64b3fec2fce295d9679cc2da0117.png/show'),
+(14, 'Hydromelon', 2, 'https://img.game8.co/3685884/8db42861f43347238fda7bc49d8839eb.png/show');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `item`
+-- Índices para tabela `item`
 --
 ALTER TABLE `item`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `item`
 --
 ALTER TABLE `item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
